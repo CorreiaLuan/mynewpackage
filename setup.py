fcb83dbd5c61a7f333cea64400088ce142094a5d
Luan Correia <luan.a.correia@live.com>
2020-09-07 13:45:40 -0300

    Updated setup.py and created my_cool_test_method

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mynewpackage',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/CorreiaLuan/mynewpackage.git',
+    author='Luan Correia',
+    author_email='luan.a.correia@live.com',
+    license='unlicense',
+    packages=['mynewpackage'],
+    zip_safe=False
+)
